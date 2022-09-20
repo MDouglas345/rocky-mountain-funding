15e5b6395e9e8b4fd508fbc8892620750efd74ed
michaeldouglas <michael_gcm@hotmail.com>
2022-09-20 09:53:03 -0600

    init; navbar work

---
+++ src/views/HomeView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <HelloWorld></HelloWorld>
+  </div>
+</template>
+
+<script>
+import HelloWorld from "@/components/HelloWorld.vue";
+// @ is an alias to /src
+
+export default {
+  name: "HomeView",
+  components: {
+    HelloWorld,
+  },
+};
+</script>
