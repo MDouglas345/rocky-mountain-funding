af48170b8274f55538fb7e5e9b8759a46dcf9a88
michaeldouglas <michael_gcm@hotmail.com>
2022-09-29 16:48:21 -0600

    navbar fixed.

--- src/components/NavigationBarContentDynamic.vue
+++ src/components/NavigationBarContentDynamic.vue
@@ -5,8 +5,12 @@
       class="navbar-nav mb-2 mb-lg-0 my-auto row "
     >
         <div v-for="(item,index) in NavbarContent" v-bind="item.tags">
-            <router-link @click="collapseNav()" v-bind:to=item.url v-bind=item.linktags >{{item.name}}</router-link>
-            <a v-if="isMobile() && item.menu.length != 0" @click="toggleDropdown()">^</a>
+          <div class = "d-flex justify-content-center position-relative">
+              <router-link @click="collapseNav()" v-bind:to=item.url v-bind=item.linktags >{{item.name}}</router-link>
+              <a v-if="isMobile() && item.menu.length != 0" @click="toggleDropdown()" class="dropdown-toggler-caret">
+                <span class="material-icons">arrow_drop_down</span>
+              </a>
+            </div>
             <NavigationBarDropDownDynamic :NavbarDropdownContent=item></NavigationBarDropDownDynamic>
         </div>
     </div>
