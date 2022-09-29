1975f5467fcf1c3e0dc2f2ca049c6cc2269fab59
michaeldouglas <michael_gcm@hotmail.com>
2022-09-28 20:44:31 -0600

    solution template styling

--- src/components/list/StepsWithDetails.vue
+++ src/components/list/StepsWithDetails.vue
@@ -1,14 +1,15 @@
 <template>
     <ol class = "OuterStepList">
         <li v-for="(item,index) in content.data">
-            <div class ="">
-                <svg preserveAspectRatio="xMidYMid meet" data-bbox="0 0 200 200" xmlns="http://www.w3.org/2000/svg" width="200" height="200" viewBox="0 0 200 200" role="presentation" aria-hidden="true">
+            <div class ="OuterStepNum position-relative">
+                <svg  class="" preserveAspectRatio="xMidYMid meet" data-bbox="0 0 200 200" xmlns="http://www.w3.org/2000/svg" width="60" height="60" viewBox="0 0 200 200" role="presentation" aria-hidden="true">
                     <g>
                         <path d="M200 100c0 55.228-44.772 100-100 100S0 155.228 0 100 44.772 0 100 0s100 44.772 100 100z"></path>
                     </g>
                 </svg>
-                {{item.name}}
+                <p class="position-absolute"></p>
             </div>
+            <p>{{item.name}}</p>
             <ul class="InnerStepList">
                 <li v-for="(step,index) in item.data">{{step.name}}</li>
             </ul>
