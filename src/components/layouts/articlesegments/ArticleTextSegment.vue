5fd35a8c30e9ad0e0c2a444e0a9680a8b5685089
michaeldouglas <michael_gcm@hotmail.com>
2022-09-29 22:31:24 -0600

    solutions page work pt 2

--- src/components/layouts/articlesegments/ArticleTextSegment.vue
+++ src/components/layouts/articlesegments/ArticleTextSegment.vue
@@ -1,9 +1,9 @@
 <template>
-    <div class = "col-md-6 col-sm-12 relative article-item mx-auto my-auto">
+    <div class = "col-md-6 col-sm-12 relative article-item mx-auto my-auto p-3">
        <div class = "row justify-content-center">
-            <div class="col-10 col-md-8 text-start">
-                <h1 class="h3 fw-bold text-primary ">{{content.header.text}}</h1>
-                <h3 v-if="content.subheader != null" class = "h5 text-primary">{{content.subheader}}</h3>
+            <div class="col-10 col-md-9 text-start">
+                <h1 :class="content.header.class">{{content.header.text}}</h1>
+                <h3 v-if="content.subheader != null" :class ="content.subheader.class" >{{content.subheader.text}}</h3>
                 <p v-html="content.body.html"></p>
                 <button v-if="content.button != null" class="btn btn-primary p-3">
                     <router-link :to="content.button.to" class="text-secondary nav-link">{{content.button.value}}</router-link>
