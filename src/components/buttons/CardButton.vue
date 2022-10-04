f29e1c24d23df6f6e1a066ecbb16c94d80a87f5e
michaeldouglas <michael_gcm@hotmail.com>
2022-10-04 16:30:16 -0600

    misc fixes

--- src/components/buttons/CardButton.vue
+++ src/components/buttons/CardButton.vue
@@ -1,13 +1,19 @@
 <template>
     
-    <router-link v-if="content.externallink==null" :to="content.link" class="">
-        <span class="material-icons home-card-button">
+    <router-link v-if="content.link!=null" :to="content.link" class="">
+        <span class="material-icons home-card-button text-primary">
             {{content.button}}
         </span> 
     </router-link>
 
     <a v-if="content.externallink!=null" :href="content.externallink" class="">
-        <span class="material-icons home-card-button">
+        <span class="material-icons home-card-button text-primary">
+            {{content.button}}
+        </span> 
+    </a>
+
+    <a v-if="content.locallink!=null" href="#" @click.prevent="goToSection(content.locallink)">
+        <span class="material-icons home-card-button text-primary">
             {{content.button}}
         </span> 
     </a>
@@ -20,6 +26,14 @@
         props : {
             content : {
                 type : Object
+            }
+        },
+        methods : {
+            goToSection(value) {
+                console.log(value);
+                const top = document.getElementById(value).scrollIntoView({
+
+                    behavior: 'smooth'});
             }
         }
     };
--- src/components/layouts/articlesegments/ArticleTextSegment.vue
+++ src/components/layouts/articlesegments/ArticleTextSegment.vue
@@ -2,8 +2,8 @@
     <div class = "col-md-6 col-sm-12 relative article-item mx-auto my-auto p-3">
        <div class = "row justify-content-center">
             <div class="col-10 col-md-9 text-start">
-                <h1 :class="content.header.class">{{content.header.text}}</h1>
-                <h3 v-if="content.subheader != null" :class ="content.subheader.class" >{{content.subheader.text}}</h3>
+                <h1 :class="content.header.class" :id="content.header.id">{{content.header.text}}</h1>
+                <h3 v-if="content.subheader != null" :class ="content.subheader.class" :id="content.subheader.id" >{{content.subheader.text}}</h3>
                 <p v-html="content.body.html"></p>
                  <router-link :to="content.button.to" class="text-secondary nav-link">
                     <button v-if="content.button != null" class="btn btn-primary p-3 text-secondary">
